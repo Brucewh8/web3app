52063e78080907852f47b974dda95130e75c491d
brucewh8 <brucewh8@gmail.com>
2023-11-29 15:23:03 +0800

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import React, { useState } from 'react';
-import Head from 'next/head';
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import React, { useState } from "react";
+import Head from "next/head";
+import axios from "axios";
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const navigation = [
   { name: 'autoMint', href: '#', current: true },
@@ -18,26 +19,44 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
 
-function Home() {
-  const [key, setKey] = useState('');
+function App() {
+  const [key, setKey] = useState(""); // 用于存储输入的key
+  const [rpc, setRpc] = useState("");
+  const [message, setMessage] = useState(""); // 用于显示响应的消息
 
-  const handleChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+
+  const handleKeyChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
     setKey(e.target.value);
   };
 
+  const handleRpcChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+    setRpc(e.target.value);
+  };
+
   const handleClick = () => {
-    // 调用另一台服务器上的mint.js脚本，传递key参数
-    fetch('http://example.com/mint.js?key=' + key)
-      .then((response) => response.json())
-      .then((data) => {
-        // 处理返回的数据
-        console.log(data);
+    // 用于处理按钮的点击
+    // 发送一个POST请求，指定服务器的ip地址，端口和路由，传递key作为参数
+    axios
+      .post("http://195.206.235.202:3001/autoMint-serv", {
+        key: key,
+        rpc: rpc,
+      })
+      .then((response) => {
+        // 处理服务器的响应
+        if (response.data.success) {
+          // 如果成功，显示结果
+          setMessage("执行成功，结果是：" + response.data.result);
+        } else {
+          // 如果失败，显示错误信息
+          setMessage("执行失败，错误信息是：" + response.data.error);
+        }
       })
       .catch((error) => {
-        // 处理错误
-        console.error(error);
+        // 处理请求的错误
+        setMessage("请求失败，错误信息是：" + error.message);
       });
   };
+
 
   return (
     <div className="min-h-full">
@@ -126,94 +145,98 @@ function Home() {
           </>
         )}
       </Disclosure>
-      <header className='bg-white shadow'>
+      <header className="bg-white">
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <h1 className="text-2xl font-bold tracking-tight text-gray-900">autoMint</h1>
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">autoMint</h1>
         </div>
       </header>
-      <main className="bg-white shadow flex flex-col items-center justify-center">
-        <div className="border-b border-gray-900/10 pb-12">
-          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-8">
-            <div className="sm:col-span-8">
-              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                私钥
-              </label>
-              <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <input
-                    type="text"
-                    placeholder="请输入key"
-                    value={key}
-                    onChange={handleChange}
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  />
+      <main className="bg-white">
+        <div className="mx-auto max-w-4xl py-6 sm:px-6 lg:px-8">
+          {/* Your content */}
+          <div className="mt-12 space-y-4">
+            <div className="border-b border-gray-900/10 pb-12">
+              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                {/* 私钥 */}
+                <div className="sm:col-span-4">
+                  <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                    私钥
+                  </label>
+                  <div className="mt-2">
+                    {/* <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"> */}
+                      <input
+                        type="text"
+                        placeholder="请输入key"
+                        value={key}
+                        onChange={handleKeyChange}
+                        className="block w-full rounded-md border-0 py-1.5 pl-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    {/* </div> */}
+                  </div>
                 </div>
+                {/* RPC */}
+                <div className="sm:col-span-4">
+                  <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                    RPC
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      placeholder="请输入RPC"
+                      value={rpc}
+                      onChange={handleRpcChange}
+                      className="block w-full rounded-md border-0 py-1.5 pl-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+                {/* 重复次数 */}
+                <div className="sm:col-span-4">
+                  <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                    执行次数
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      placeholder="请输入执行次数"
+                      // value={key}
+                      // onChange={handleChange}
+                      className="block w-full rounded-md border-0 py-1.5 pl-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+                
               </div>
             </div>
-            <div className="sm:col-span-8">
-              <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
-                RPC
-              </label>
-              <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  {/* <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span> */}
-                  <input
-                    type="text"
-                    placeholder="请输入RPC"
-                    // value={key}
-                    // onChange={handleChange}
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-              {/* <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p> */}
+            <div className="mt-6 flex items-center justify-end gap-x-6">
+              <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+                Cancel
+              </button>
+              <button
+                onClick={handleClick}
+                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                启动脚本
+              </button>
             </div>
-            <div className="sm:col-span-4">
+            <div className="col-span-full">
               <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
-                重复次数
+                Output
               </label>
               <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  {/* <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span> */}
-                  <input
-                    type="text"
-                    placeholder=""
-                    // value={key}
-                    // onChange={handleChange}
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  />
-                </div>
+                <textarea
+                  value={message}
+                  id="output"
+                  name="output"
+                  rows={8}
+                  className="block w-full rounded-md border-0 py-1.5 pl-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
+                  readOnly
+                />
               </div>
-              {/* <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p> */}
             </div>
           </div>
-        </div>
-        <div className="mt-6 flex items-center justify-end gap-x-6">
-          <button
-            onClick={handleClick}
-            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            启动脚本
-          </button>
-        </div>
-        <div className="col-span-8">
-          <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
-            Output
-          </label>
-          <div className="mt-2">
-            <textarea
-              id="about"
-              name="about"
-              rows={8}
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              defaultValue={''}
-            />
-          </div>
-          <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
         </div>
       </main>
     </div>
   );
 }
 
-export default Home;
+export default App;
